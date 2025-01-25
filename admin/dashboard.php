da548b8c3acfd3d34862874e43ac0cd627b050cd
Windel Navales <windelvlog@gmail.com>
2025-01-25 09:37:25 +0800

    DYNAMIC

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -63,107 +63,39 @@
 
                 <!-- Content Cards -->
                 <div class="row">
-    <!-- Employees Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-primary h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-dark fw-bolder text-uppercase mb-1">
-                            Employees</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">1000</div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="bi bi-person-lines-fill fa-3x text-gray-300 opacity-75" style="font-size: 50px;"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <!-- Schools Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-success h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-dark fw-bolder text-uppercase mb-1">
-                            Schools</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">1000</div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="bi bi-building fa-3x text-gray-300 opacity-75" style="font-size: 50px;"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                <?php 
 
-    <!-- Subjects Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-info h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-dark fw-bolder text-uppercase mb-1">
-                            Subjects</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">50</div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="bi bi-book fa-3x text-gray-300 opacity-75" style="font-size: 50px;"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+if(isset($_GET['page'])){
+    $page = $_GET['page'];
+    switch($page){
+        case 'dashboard':
+            include 'includes/dashboard.php';
+            break;
+        case 'employee':
+            include 'includes/employee.php';
+            break;
+        case 'records':
+            include 'includes/records.php';
+            break;
+        case 'account':
+            include 'includes/account.php';
+            break;
+        default:
+            include 'includes/dashboard.php';
+            break;
+    }
 
-    <!-- Positions Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-warning h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-dark fw-bolder text-uppercase mb-1">
-                            Positions</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">30</div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="bi bi-person-badge fa-3x text-gray-300 opacity-75" style="font-size: 50px;"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+}
+?>
+   
 </div>
 
  </div>
         </div>
     </div>
 
-            <?php 
-
-            if(isset($_GET['page'])){
-                $page = $_GET['page'];
-                switch($page){
-                    case 'dashboard':
-                        include 'includes/dashboard.php';
-                        break;
-                    case 'employee':
-                        include 'includes/employee.php';
-                        break;
-                    case 'records':
-                        include 'includes/records.php';
-                        break;
-                    case 'account':
-                        include 'includes/account.php';
-                        break;
-                    default:
-                        include 'includes/dashboard.php';
-                        break;
-                }
-
-
-            ?>
+      
 
 
     <!-- Scripts -->
